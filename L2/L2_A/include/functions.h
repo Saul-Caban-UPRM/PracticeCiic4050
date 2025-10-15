fdac331e11495a30d465fe6d0ea156286027fadf
Saul-Caban-UPRM <saul.caban@upr.edu>
2025-10-15 13:37:05 -0400

    working

---
+++ L2/L2_A/include/functions.h
@@ -0,0 +1,5 @@
+
+#ifndef INCLUDE_FUNCTIONS_H_
+#define INCLUDE_FUNCTIONS_H_
+
+#endif  // INCLUDE_FUNCTIONS_H_
---
+++ L2/L2_A/src/main.c
@@ -0,0 +1,77 @@
+#define _POSIX_C_SOURCE 200809L
+#include <fcntl.h>
+#include <math.h>
+#include <mqueue.h>
+#include <pthread.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+int total_count = 0;
+
+int64_t total_sum = 0;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
+int Is_Prime(int n) {
+  if (n < 2) return 0;
+  for (int i = 2; i <= sqrt(n); i++) {
+    if (n % i == 0) return 0;
+  }
+  return 1;
+}
+
+void* sum_primes(void* arg) {
+  int thread_id = *(int*)arg;
+  free(arg);  // Free the allocated memory
+  int start;
+  if (thread_id == 0) {
+    start = 2;
+  } else {
+    start = 500001;
+  }
+  int end;
+  if (thread_id == 0) {
+    end = 500000;
+  } else {
+    end = 1000000;
+  }
+
+  int64_t local_sum = 0;
+  int local_count = 0;
+  for (int i = start; i <= end; i++) {
+    if (Is_Prime(i)) {
+      local_sum += i;
+      local_count++;
+    }
+  }
+
+  pthread_mutex_lock(&lock);
+  total_sum += local_sum;
+  total_count += local_count;
+  pthread_mutex_unlock(&lock);
+
+  return NULL;
+}
+
+int main() {
+  pthread_t Threads[2];
+
+  for (int i = 0; i < 2; i++) {
+    int* id = malloc(sizeof(int));
+    *id = i;
+    pthread_create(&Threads[i], NULL, sum_primes, id);
+  }
+
+  for (int i = 0; i < 2; i++) {
+    pthread_join(Threads[i], NULL);
+  }
+
+  printf("Total sum of primes between 1 and 1,000,000: %ld\n", total_sum);
+  printf("Total number of primes between 1 and 1,000,000: %d\n", total_count);
+  return 0;
+}
